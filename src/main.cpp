c9923a46aa98eb80e0968e78d0d52a360e7ee424
Daniel Santos <daninsky@protonmail.com>
2021-09-06 20:46:12 -0300

    Intro to C Day 4.

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,36 @@
 
 std::string version();
 
+struct alignas(32) projectile {
+    // These are the members of this structure
+    char unsigned is_on_fire;
+    int damage;
+    int particles_per_second;
+    short how_many;
+} hello[3];     // this define an array of projectiles right after the definition
+
 int WinMain(HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     LPSTR lpCmdLine,
     int nCmdShow)
 {
-    char unsigned test;
-    char unsigned* p_test;
-    p_test = &test;
+    unsigned short test;
 
-    // x86 overflows wrap around numbers
-    test = 255;
-    test = test + 1;
+    hello[1].is_on_fire = true;
+
+    projectile project;
+    project.is_on_fire = 1;
+    project.damage = 100;
+    project.particles_per_second = 200;
+    project.how_many = 4;
+    
+    size_t char_size = sizeof(char);
+    size_t int_size = sizeof(int);
+    size_t short_size = sizeof(short);
+    
+    size_t projectile_size = sizeof(projectile);    // 16?
+
+
 }
 
 std::string version()
