bbd3725035612d244f7559098bcff1f9b0bd56bb
Daniel Santos <daninsky@protonmail.com>
2021-09-08 16:30:24 -0300

    Day 3
    Graphics backbuffer

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,83 @@
 #include <windows.h>
 #include "handmade_hero_config.h"
 
-LRESULT CALLBACK win_procedure_cb(HWND   window,
-                                  UINT   message,
-                                  WPARAM w_parameter,
-                                  LPARAM l_parameter)
+// TODO(casey): This is a global for now.
+static bool running;
+
+static BITMAPINFO bitmap_info;
+static void* bitmap_memory;
+static HBITMAP bitmap_handle;
+static HDC bitmap_device_context;
+
+static void win32_resize_dib_section(int w, int h)
+{
+    // TODO(casey): Bulletproof this.
+    // Maybe don't free first, free after, then free first if that fails.
+
+    // TODO(casey): Free our DIBSection;
+    
+    if (bitmap_handle) {
+        DeleteObject(bitmap_handle);
+    }
+
+    if (!bitmap_device_context) {
+        // TODO(casey): Should we recreate these under certain special circumstances?
+        bitmap_device_context = CreateCompatibleDC(0);
+    }
+
+    bitmap_info.bmiHeader.biSize = sizeof(bitmap_info.bmiHeader);
+    bitmap_info.bmiHeader.biWidth = w;
+    bitmap_info.bmiHeader.biHeight = h;
+    bitmap_info.bmiHeader.biPlanes = 1;
+    bitmap_info.bmiHeader.biBitCount = 32;
+    bitmap_info.bmiHeader.biCompression = BI_RGB;
+    
+    bitmap_handle = CreateDIBSection(
+        bitmap_device_context, &bitmap_info,
+        DIB_RGB_COLORS, &bitmap_memory, 0, 0);   
+}
+
+static void win32_update_window(HDC device_context, int x, int y,
+                                int width, int height)
+{
+    StretchDIBits(
+        device_context,
+        x, y, width, height,
+        x, y, width, height,
+        bitmap_memory,
+        &bitmap_info,
+        DIB_RGB_COLORS,
+        SRCCOPY
+    );
+}
+
+LRESULT CALLBACK win32_main_window_proc_cb(HWND   window,
+                                           UINT   message,
+                                           WPARAM w_parameter,
+                                           LPARAM l_parameter)
 {
     LRESULT result = 0;
-    
-    
 
     switch (message) {
     case WM_SIZE:
+    {
+        RECT client_rect;
+        GetClientRect(window, &client_rect);
+        int width = client_rect.right - client_rect.left;
+        int height = client_rect.bottom - client_rect.top;
+        win32_resize_dib_section(width, height);
         OutputDebugString("WM_SIZE\n");
         break;
-    case WM_DESTROY:
-        OutputDebugString("WM_DESTROY\n");
-        break;
+
+    }
     case WM_CLOSE:
+        // TODO:(casey): Handle this with a message to the user?
+        running = false;
         OutputDebugString("WM_CLOSE\n");
+        break;
+    case WM_DESTROY:
+        // TODO(casey): Handle this as an error - recreate window?
+        running = false;
         break;
     case WM_ACTIVATEAPP:
         OutputDebugString("WM_ACTIVATEAPP\n");
@@ -33,12 +92,9 @@ LRESULT CALLBACK win_procedure_cb(HWND   window,
         LONG y = paint.rcPaint.top;
         LONG width = paint.rcPaint.right - paint.rcPaint.left;
         LONG height = paint.rcPaint.bottom - paint.rcPaint.top;
+        win32_update_window(device_context, x, y, width, height);
         static DWORD color = WHITENESS;
-        if (color == WHITENESS)
-            color = BLACKNESS;
-        else
-            color = WHITENESS;
-        PatBlt(device_context, x, y, width, height, color);
+
         EndPaint(window, &paint);
     }
     default:
@@ -58,7 +114,7 @@ int CALLBACK WinMain(HINSTANCE instance,
     
     // TODO:(casey): Check if HREDRAW/VREDRAW/OWNDC still matter
     window_class.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
-    window_class.lpfnWndProc = win_procedure_cb;    // handle windows messages
+    window_class.lpfnWndProc = win32_main_window_proc_cb;    // handle windows messages
     window_class.hInstance = instance;
 
     // TODO(daniel): Check for hCursor for inkbreaker
@@ -82,8 +138,10 @@ int CALLBACK WinMain(HINSTANCE instance,
             nullptr
         );
         if (window_handle) {
+            running = true;
             MSG message;
-            while (true) {
+            
+            while (running) {
                 BOOL message_result = GetMessage(&message, 0, 0, 0);
                 if (message_result > 0) {
                     TranslateMessage(&message);
