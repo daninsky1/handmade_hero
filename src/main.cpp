3e5b9129bb2687b11b4e7f53fdcb0a7787e1a1ea
Daniel Santos <daninsky@protonmail.com>
2021-09-07 23:46:53 -0300

    Handmade Hero DAY 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,105 @@
+#include <iostream>
 #include <sstream>
 #include <windows.h>
 #include "handmade_hero_config.h"
 
-std::string version();
-
-struct alignas(32) projectile {
-    // These are the members of this structure
-    char unsigned is_on_fire;
-    int damage;
-    int particles_per_second;
-    short how_many;
-} hello[3];     // this define an array of projectiles right after the definition
-
-int WinMain(HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine,
-    int nCmdShow)
+LRESULT CALLBACK win_procedure_cb(HWND   window,
+                                  UINT   message,
+                                  WPARAM w_parameter,
+                                  LPARAM l_parameter)
 {
-    unsigned short test;
-
-    hello[1].is_on_fire = true;
-
-    projectile project;
-    project.is_on_fire = 1;
-    project.damage = 100;
-    project.particles_per_second = 200;
-    project.how_many = 4;
+    LRESULT result = 0;
     
-    size_t char_size = sizeof(char);
-    size_t int_size = sizeof(int);
-    size_t short_size = sizeof(short);
     
-    size_t projectile_size = sizeof(projectile);    // 16?
 
+    switch (message) {
+    case WM_SIZE:
+        OutputDebugString("WM_SIZE\n");
+        break;
+    case WM_DESTROY:
+        OutputDebugString("WM_DESTROY\n");
+        break;
+    case WM_CLOSE:
+        OutputDebugString("WM_CLOSE\n");
+        break;
+    case WM_ACTIVATEAPP:
+        OutputDebugString("WM_ACTIVATEAPP\n");
+        break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT paint;
+        HDC device_context = BeginPaint(window, &paint);
+        LONG x = paint.rcPaint.left;
+        LONG y = paint.rcPaint.top;
+        LONG width = paint.rcPaint.right - paint.rcPaint.left;
+        LONG height = paint.rcPaint.bottom - paint.rcPaint.top;
+        static DWORD color = WHITENESS;
+        if (color == WHITENESS)
+            color = BLACKNESS;
+        else
+            color = WHITENESS;
+        PatBlt(device_context, x, y, width, height, color);
+        EndPaint(window, &paint);
+    }
+    default:
+        OutputDebugString("default\n");
+        result = DefWindowProc(window, message, w_parameter, l_parameter);
+        break;
+    }
 
+    return result;
 }
 
-std::string version()
+int CALLBACK WinMain(HINSTANCE instance,
+                     HINSTANCE, LPSTR lpCmdLine,
+                     int nCmdShow)
 {
-    std::stringstream ss;
-    ss << "Handmade Hero VERSION "
-       << HANDMADE_HERO_VERSION_MAJOR << '.'
-       << HANDMADE_HERO_VERSION_MINOR << '.'
-       << HANDMADE_HERO_VERSION_PATCH << std::endl;
-    return ss.str();
+    WNDCLASS window_class = {};
+    
+    // TODO:(casey): Check if HREDRAW/VREDRAW/OWNDC still matter
+    window_class.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    window_class.lpfnWndProc = win_procedure_cb;    // handle windows messages
+    window_class.hInstance = instance;
+
+    // TODO(daniel): Check for hCursor for inkbreaker
+    // window_class.hCursor;
+    // window_class.lpszMenuName;
+    window_class.lpszClassName = "HandmadeHeroWindowClass";
+    
+    if (RegisterClass(&window_class)) {
+        HWND window_handle = CreateWindowExA(
+            0,
+            window_class.lpszClassName,
+            "Handmade Hero",
+            WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            0,
+            0,
+            instance,
+            nullptr
+        );
+        if (window_handle) {
+            MSG message;
+            while (true) {
+                BOOL message_result = GetMessage(&message, 0, 0, 0);
+                if (message_result > 0) {
+                    TranslateMessage(&message);
+                    DispatchMessage(&message);
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        else {
+            // TODO(casey): Logging
+        }
+    }
+    else {
+        // TODO(casey): Logging
+    }
+
 }
