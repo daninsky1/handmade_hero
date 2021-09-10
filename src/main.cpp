a1cda0cc31fd169f5cef921f285093354c1ed6de
Daniel Santos <daninsky@protonmail.com>
2021-09-10 11:34:57 -0300

    Day 6
    Xinput basic setup. And Joystick basic setup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
+#include <string>
 #include <sstream>
+#include <cstdint>
+
 #include <windows.h>
+#include <Xinput.h>         // XBox joystick controller
+#include <joystickapi.h>    // Generic joystick controller
+
 #include "handmade_hero_config.h"
 
-// TODO(casey): This is a global for now.
+// NOTE(casey): XInputGetState
+#define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE* pState)
+typedef X_INPUT_GET_STATE(x_input_get_state);
+X_INPUT_GET_STATE(XInputGetStateStub)
+{
+    return(ERROR_DEVICE_NOT_CONNECTED);
+}
+static x_input_get_state* XInputGetState_ = XInputGetStateStub;
+#define XInputGetState XInputGetState_
+
+// NOTE(casey): XInputSetState
+#define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION* pVibration)
+typedef X_INPUT_SET_STATE(x_input_set_state);
+X_INPUT_SET_STATE(XInputSetStateStub)
+{
+    return(ERROR_DEVICE_NOT_CONNECTED);
+}
+static x_input_set_state* XInputSetState_ = XInputSetStateStub;
+#define XInputSetState XInputSetState_
+
+static void win32_load_xinput()
+{
+    HMODULE xinput_library = LoadLibraryA("xinput1_3.dll");
+    if (xinput_library) {
+         XInputGetState = reinterpret_cast<x_input_get_state*>(GetProcAddress(xinput_library, "XInputGetState"));
+         XInputSetState = reinterpret_cast<x_input_set_state*>(GetProcAddress(xinput_library, "XInputSetState"));
+    }
+}
 
 struct Win32OffscreenBuffer {
+    // NOTE(casey): Pixels are always 32-bits wide, memory order BB GG RR XX
     BITMAPINFO info;
     void* memory;
     int width;
@@ -22,7 +56,12 @@ struct Win32WindowDimension {
     int height;
 };
 
-Win32WindowDimension win32_get_window_dimension(HWND window)
+// NOTE(daniel): I got to make globally available for windows procedure get access
+// to this
+static int xoff = 0;
+static int yoff = 0;
+
+static Win32WindowDimension win32_get_window_dimension(HWND window)
 {
     Win32WindowDimension result;
     RECT client_rect;
@@ -33,7 +72,7 @@ Win32WindowDimension win32_get_window_dimension(HWND window)
     return result;
 }
 
-static void render_test_gradient(Win32OffscreenBuffer buffer, int xoff, int yoff)
+static void render_test_gradient(Win32OffscreenBuffer& buffer, int xoff, int yoff)
 {
     int w = buffer.width;
     int h = buffer.height;
@@ -83,9 +122,7 @@ static void win32_resize_dib_section(Win32OffscreenBuffer& buffer, int w, int h)
 
 static void win32_display_buffer_in_window(HDC device_context,
                                            int window_width, int window_height,
-                                           Win32OffscreenBuffer buffer,
-                                           int x, int y,
-                                           int width, int height)
+                                           Win32OffscreenBuffer& buffer)
 {
     // TODO:(casey): Aspect ration correction
     StretchDIBits(device_context,
@@ -119,9 +156,59 @@ LRESULT CALLBACK win32_main_window_proc_cb(HWND   window,
         // TODO(casey): Handle this as an error - recreate window?
         running = false;
         break;
-    case WM_ACTIVATEAPP:
-        OutputDebugString("WM_ACTIVATEAPP\n");
+    case WM_SYSKEYDOWN: case WM_SYSKEYUP:
+    case WM_KEYDOWN:    case WM_KEYUP:
+    {
+        uint32_t vk_code = w_parameter;
+        bool was_down = (l_parameter & (1 << 30)) != 0;
+        bool is_down = (l_parameter & (1 << 31)) == 0;
+        if (was_down != is_down) {
+            if (vk_code == 'W') {
+                OutputDebugString("W\n");
+            }
+            else if (vk_code == 'A') {
+                OutputDebugString("A\n");
+            }
+            else if (vk_code == 'S') {
+                OutputDebugString("S\n");
+            }
+            else if (vk_code == 'D') {
+                OutputDebugString("D\n");
+            }
+            else if (vk_code == 'Q') {
+                OutputDebugString("Q\n");
+            }
+            else if (vk_code == 'E') {
+                OutputDebugString("E\n");
+            }
+            else if (vk_code == VK_UP) {
+                OutputDebugString("UP\n");
+            }
+            else if (vk_code == VK_DOWN) {
+                OutputDebugString("DOWN\n");
+            }
+            else if (vk_code == VK_LEFT) {
+                OutputDebugString("LEFT\n");
+            }
+            else if (vk_code == VK_RIGHT) {
+                OutputDebugString("RIGHT\n");
+            }
+            else if (vk_code == VK_ESCAPE) {
+                OutputDebugString("ESCAPE: ");
+                if (is_down) {
+                    OutputDebugString("is_down ");
+                }
+                if (was_down) {
+                    OutputDebugString("was_down");
+                }
+                OutputDebugString("\n");
+            }
+            else if (vk_code == VK_SPACE) {
+                OutputDebugString("SPACE\n");
+            }
+        }
         break;
+    }
     case WM_PAINT:
     {
         PAINTSTRUCT paint;
@@ -132,14 +219,14 @@ LRESULT CALLBACK win32_main_window_proc_cb(HWND   window,
         LONG height = paint.rcPaint.bottom - paint.rcPaint.top;
 
         Win32WindowDimension dimension = win32_get_window_dimension(window);
-        win32_display_buffer_in_window(device_context, dimension.width, dimension.height, global_backbuffer, x, y, width, height);
+        win32_display_buffer_in_window(device_context, dimension.width, dimension.height, global_backbuffer);
         static DWORD color = WHITENESS;
 
         EndPaint(window, &paint);
         break;
     }
     default:
-        OutputDebugString("default\n");
+        //OutputDebugString("default\n");
         result = DefWindowProc(window, message, w_parameter, l_parameter);
         break;
     }
@@ -151,11 +238,12 @@ int CALLBACK WinMain(HINSTANCE instance,
                      HINSTANCE, LPSTR lpCmdLine,
                      int nCmdShow)
 {
+    win32_load_xinput();
     // NOTE(daniel): Resolution
     int w = 1280;
     int h = 720;
     win32_resize_dib_section(global_backbuffer, w, h);
-    WNDCLASS window_class = {};
+    WNDCLASSA window_class = {};
     
     // TODO:(casey): Check if HREDRAW/VREDRAW/OWNDC still matter
     window_class.style = CS_HREDRAW|CS_VREDRAW;
@@ -186,8 +274,30 @@ int CALLBACK WinMain(HINSTANCE instance,
             running = true;
             MSG message;
             
-            int xoff = 0;
-            int yoff = 0;
+            // NOTE(daniel): GENERIC JOYSTICK CONTROLLER
+            // NOTE(daniel): eventually replace joystick input with DirectInput
+            JOYINFOEX joyinfoex;
+            UINT joystick_id = JOYSTICKID1;
+            JOYCAPS joy_capabilities;
+            BOOL dev_attached;
+            // NOTE(daniel): Number of joystick supported by the system drive,
+            // however doesn't indicate the number of joystick attached to the system
+            UINT num_dev = joyGetNumDevs();
+            if (num_dev) {
+                std::string num_dev_msg;
+                num_dev_msg.append(std::to_string(num_dev));
+                OutputDebugString("16 Joysticks supported");
+            }
+            else {
+                // TODO(daniel): LOG no joystick driver supported
+            }
+            
+            if (joyGetPosEx(JOYSTICKID1, &joyinfoex) != JOYERR_UNPLUGGED) {
+                joystick_id = JOYSTICKID1;
+                joyGetDevCaps(joystick_id, &joy_capabilities, sizeof(joy_capabilities));
+            }
+
+
             while (running) {
                 // NOTE(daniel): Flush queue
                 while (PeekMessage(&message, 0, 0, 0, PM_REMOVE)) {
@@ -195,14 +305,87 @@ int CALLBACK WinMain(HINSTANCE instance,
                     TranslateMessage(&message);
                     DispatchMessage(&message);
                 }
+
+                // TODO(casey): Should we poll this more frequently?
+                for (DWORD i = 0; i < XUSER_MAX_COUNT; ++i) {
+                    XINPUT_STATE controller_state;
+                    if (XInputGetState(i, &controller_state) == ERROR_SUCCESS) {
+                        // NOTE(casey): This controller is plugged in
+                        // TODO(casey): See if controller_state.dwPacketNumber increments to rapidly
+                        XINPUT_GAMEPAD* pad = &controller_state.Gamepad;
+                        bool gpad_up         = pad->wButtons & XINPUT_GAMEPAD_DPAD_UP;
+                        bool gpad_down       = pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN;
+                        bool gpad_right      = pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT;
+                        bool gpad_left       = pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT;
+                        bool gpad_start      = pad->wButtons & XINPUT_GAMEPAD_START;
+                        bool gpad_back       = pad->wButtons & XINPUT_GAMEPAD_BACK;
+                        bool gpad_lthumb     = pad->wButtons & XINPUT_GAMEPAD_LEFT_THUMB;
+                        bool gpad_rthumb     = pad->wButtons & XINPUT_GAMEPAD_RIGHT_THUMB;
+                        bool gpad_lshoulder  = pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER;
+                        bool gpad_rshoulder  = pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER;
+                        bool gpad_a          = pad->wButtons & XINPUT_GAMEPAD_A;
+                        bool gpad_b          = pad->wButtons & XINPUT_GAMEPAD_B;
+                        bool gpad_x          = pad->wButtons & XINPUT_GAMEPAD_X;
+                        bool gpad_y          = pad->wButtons & XINPUT_GAMEPAD_Y;
+                        // Gamepad left stick x and y
+                        int16_t gpad_lstickx = pad->sThumbLX;
+                        int16_t gpad_lsticky = pad->sThumbLY;
+
+                        xoff += gpad_lstickx >> 12;
+                        yoff += gpad_lsticky >> 12;
+                    }
+                    else {
+                        // NOTE(casey): The controller is not available 
+                    }
+                }
+                // Test vibration
+                XINPUT_VIBRATION vibration;
+                vibration.wLeftMotorSpeed = INT16_MAX;
+                vibration.wRightMotorSpeed = INT16_MAX;
+                XInputSetState(0, &vibration);
+
+                // NOTE(daniel): my generic controller input
+                // NOTE(daniel): This is playstation 2 nomenclatures
+                joyinfoex.dwSize = sizeof(joyinfoex);
+                joyinfoex.dwFlags = JOY_RETURNBUTTONS|JOY_RETURNX|JOY_RETURNY|JOY_RETURNPOV;
+                if (joyGetPosEx(joystick_id, &joyinfoex) != JOYERR_UNPLUGGED) {
+                    // TODO(daniel): BUG: limited dpad directions
+                    // Joystick dpad
+                    bool generic_gpad_down  = joyinfoex.dwPOV == JOY_POVBACKWARD;
+                    bool generic_gpad_up    = joyinfoex.dwPOV == JOY_POVFORWARD;
+                    bool generic_gpad_left  = joyinfoex.dwPOV == JOY_POVLEFT;
+                    bool generic_gpad_right = joyinfoex.dwPOV == JOY_POVRIGHT;
+                    // Joystick buttons
+                    bool generic_gpad_tri   = joyinfoex.dwButtons & JOY_BUTTON1;
+                    bool generic_gpad_cir   = joyinfoex.dwButtons & JOY_BUTTON2;
+                    bool generic_gpad_x     = joyinfoex.dwButtons & JOY_BUTTON3;
+                    bool generic_gpad_sq    = joyinfoex.dwButtons & JOY_BUTTON4;
+                    bool generic_gpad_l2    = joyinfoex.dwButtons & JOY_BUTTON5;
+                    bool generic_gpad_r2    = joyinfoex.dwButtons & JOY_BUTTON6;
+                    bool generic_gpad_l1    = joyinfoex.dwButtons & JOY_BUTTON7;
+                    bool generic_gpad_r1    = joyinfoex.dwButtons & JOY_BUTTON8;
+                    // Joystick left analog x and y
+                    uint16_t u_generic_gpad_lanalogx = joyinfoex.dwXpos;    // unsigned values
+                    uint16_t u_generic_gpad_lanalogy = joyinfoex.dwYpos;
+                    int16_t s_generic_gpad_lanalogx = UINT16_MAX / 2 - u_generic_gpad_lanalogx;   // signed values
+                    int16_t s_generic_gpad_lanalogy = UINT16_MAX / 2 - u_generic_gpad_lanalogy;
+
+                    std::stringstream ss;
+                    ss << "unsigned x: " << u_generic_gpad_lanalogx << ", y: " << u_generic_gpad_lanalogy
+                        << " | signed x: " << s_generic_gpad_lanalogx << ", y: " << s_generic_gpad_lanalogy << std::endl;
+                    OutputDebugString(ss.str().c_str());
+                    
+                    xoff += s_generic_gpad_lanalogx >> 12;
+                    yoff += s_generic_gpad_lanalogy >> 12;
+                }
+
+
                 render_test_gradient(global_backbuffer, xoff, yoff);
 
                 HDC device_context = GetDC(window);
                 Win32WindowDimension dimension = win32_get_window_dimension(window);
-                win32_display_buffer_in_window(device_context, dimension.width, dimension.height, global_backbuffer, 0, 0, dimension.width, dimension.height);
+                win32_display_buffer_in_window(device_context, dimension.width, dimension.height, global_backbuffer);
                 ReleaseDC(window, device_context);
-
-                ++xoff;
             }
         }
         else {
